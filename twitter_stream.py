b0fd9073b89a4fc64aba3cb267ac89df02a22d05
Kryptic Mind <krypticmind@unseen.is>
2015-03-15 23:43:12 +0300

    [NOT_ISSUE] - added twitter-streaming script

---
+++ twitter_stream.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+__author__ = 'Kryptic Mind'
+
+from tweepy import OAuthHandler
+from tweepy import Stream
+from tweepy.streaming import StreamListener
+
+CONSUMER_KEY = ""
+CONSUMER_SECRET = ""
+
+ACCESS_TOKEN = ""
+ACCESS_TOKEN_SECRET = ""
+
+class Listener(StreamListener):
+    def on_data(self, data):
+        print data
+        return True
+
+    def on_error(self, status_code):
+        print status_code
+
+if __name__ == '__main__':
+    auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+    twitterStream = Stream(auth, Listener())
+    twitterStream.filter(track=["music"])
