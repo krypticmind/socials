4895c1c48f1f5c0360f5a99e16169010884ff83f
Kryptic Mind <krypticmind@unseen.is>
2015-03-27 00:57:21 +0300

    removed unnecessary code

--- twitter_stream.py
+++ twitter_stream.py
@@ -4,7 +4,6 @@
 __author__ = 'Kryptic Mind'
 
 from ConfigParser import ConfigParser
-from tweepy.streaming import StreamListener
 import daemon
 
 config = ConfigParser()
@@ -14,14 +13,6 @@ CONSUMER_KEY = config.get("ConsumerTokens", "CONSUMER_KEY")
 CONSUMER_SECRET = config.get("ConsumerTokens", "CONSUMER_SECRET")
 ACCESS_TOKEN = config.get("AccessTokens", "ACCESS_TOKEN")
 ACCESS_TOKEN_SECRET = config.get("AccessTokens", "ACCESS_TOKEN_SECRET")
-
-class Listener(StreamListener):
-    def on_data(self, data):
-        print data
-        return True
-
-    def on_error(self, status_code):
-        print status_code
 
 if __name__ == '__main__':
     bot = daemon.TwitterBot(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
