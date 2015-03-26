1b2dd467e5157cb89d25778d04a58397ee92ad8a
Kryptic Mind <krypticmind@unseen.is>
2015-03-27 00:17:36 +0300

    daemonizing

--- daemon.py
+++ daemon.py
@@ -4,18 +4,5 @@ __author__ = 'Kryptic Mind'
 
 import daemon
 
-class TwitterBot():
-
-    def __init__(self):
-        self.stdin_path = '/dev/null'
-        self.stdout_path = '/dev/tty'
-        self.stderr_path = '/dev/tty'
-        self.pidfile_path = '/var/run/twitter_bot.pid'
-        self.pidfile_timeout = 5
-
-    def run(self):
-        pass
-
-app = TwitterBot()
-daemon_runner = daemon.runner.DaemonRunner(app)
-daemon_runner.do_action()
+with daemon.DaemonContext():
+    pass
